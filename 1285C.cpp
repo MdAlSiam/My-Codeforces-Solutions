4704226ce9761be4618b3d8437385531fbc1b995
Md. Al Siam <59497423+MdAlSiam@users.noreply.github.com>
2020-01-10 23:36:28 +0600

    Create 1285C.cpp

---
+++ 1285C.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define pb push_back
+#define pll pair <ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005 ///2x10^5
+//#define maxn 1000006 ///10^6
+//#define maxn 1000000009 ///10^9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+ll n, m, k;
+ll a, b, c;
+ll x, y, z;
+ll t, test, temp;
+
+ll lcm(ll a, ll b){
+    return (a*b)/__gcd(a, b);
+}
+
+int main(){//scl(test); while(test--){
+while(scl(n) == 1){
+
+    ll ansu = inf, ansv = inf;
+
+    for(ll i = 1; i*i <= n; i++){
+        ll u, v;
+        if(n%i != 0) continue;
+        u = i, v = n/i;
+        if(v < ansv && lcm(u,v) == n){
+            ansu = u;
+            ansv = v;
+        }
+    }
+
+    printf("%lld %lld\n", ansu, ansv);
+}
+}//}

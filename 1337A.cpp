03c3319a1fad1323d2733d5d2e77da47f4365224
Md. Al Siam <highambitioussiam@gmail.com>
2020-07-02 23:17:03 +0600

    Create 1337A.cpp

---
+++ 1337A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int test;
+    cin >> test;
+    
+    while(test--){
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << b << " " << c << " " << c << endl;
+    }
+}

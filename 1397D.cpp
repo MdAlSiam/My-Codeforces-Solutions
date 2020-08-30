1b0ca0162ed4d87c32e2b55b10fb964182bb85fc
Md. Al Siam <highambitioussiam@gmail.com>
2020-08-31 03:32:02 +0600

    Add files via upload

---
+++ 1397D.cpp
@@ -0,0 +1,88 @@
+ /* In the name of Allah SWT */
+
+using namespace std;
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define ON(n,i) (n|(1LL<<i))
+#define OFF(n,i) (n&(~(1LL<<i)))
+#define CHK(n,i) (n&(1LL<<i))
+
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+#define prys printf("YES\n")
+#define prno printf("NO\n")
+
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
+
+#define pb push_back
+#define pll pair <ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005 ///2x10^5 // n*log n in 1 sec
+//#define maxn 1000006 ///10^6
+//#define maxn 1000000009 ///10^9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+/*******************************************************************************************/
+
+ll t, test, temp;
+ll n, m, k, kount;
+ll a, b, c, ans;
+ll x, y, z = -1, maxi, mini;
+ll ara[110];
+/*_________________________________________________________________________________________*/
+
+void solve(){
+    scl(n);
+    For(i, 1, n+1) scl(ara[i]);
+
+    kount = 0;
+    ll blockedindex = -1;
+
+    while(true){
+        maxi = -1;
+        ll maxindex = -1;
+
+        For(i, 1, n+1){
+            if(ara[i] >= maxi and ara[i] > 0 and i != blockedindex){
+                maxi = ara[i];
+                maxindex = i;
+            }
+        }
+
+        if(maxindex == -1) break;
+
+        ara[maxindex]--;
+        blockedindex = maxindex;
+        kount++;
+
+//        For(i, 1, n+1) printf("%lld ", ara[i]);
+//        printf("\n");
+    }
+
+    if(kount & 1) printf("T\n");
+    else printf("HL\n");
+}
+
+int main(){
+    test = 1;
+    while(scl(test) == 1)
+    while(test--) solve();
+}
+
+/*******************************************************************************************/
+
